a499ce015fee71da839eab74a0c2fa8202f80bd5
ishanhimalka <ishanhimalka1@gmail.com>
2024-02-01 19:26:07 +0100

    set firebase

--- javascript/api.js
+++ javascript/api.js
@@ -1,25 +1,2 @@
 const apiUrl = 'https://opentdb.com/api.php?amount=40&category=18&difficulty=medium&type=multiple';
 
-
-const contactForm = document.getElementById('form');
-
-contactForm.addEventListener('submit', function (event) {
-    event.preventDefault();
-    console.log("adsa");
-    fetch(apiUrl)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            
-            return response.json();
-        })
-        .then(data => {
-            for (let index = 0; index < data.results.length; index++) {
-                console.log(data.results[index]);
-            }
-         })
-        .catch(error => {
-            console.error('Error:', error);
-        });
-});
